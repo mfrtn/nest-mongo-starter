46e0fcf3b6965d365ac5962ad2e4c7c606a3bc04
m-foroutan <m.foroutan26@yahoo.com>
2023-05-29 15:03:52 +0330

    Install JWT and config JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('TOKEN_SECRET_KEY'),
+        signOptions: {
+          expiresIn: configService.get<number>('TOKEN_EXPIRE_TIME'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
